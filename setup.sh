a3bb9c95f15ffda4b5facf764d9912ef688d8ef4
Jay Clifford <jclifford@influxdata.com>
2023-07-27 14:02:41 +0100

    updated setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-mkdir -p ./grafana/plugins
-
-curl -L https://github.com/influxdata/grafana-flightsql-datasource/releases/download/v0.1.5/influxdata-flightsql-datasource-0.1.5.zip -o ./grafana/plugins/influxdata-flightsql-datasource-0.1.5.zip
-unzip ./grafana/plugins/influxdata-flightsql-datasource-0.1.5.zip -d ./grafana/plugins/
 
 echo "Running training instance...."
 docker-compose up -d
